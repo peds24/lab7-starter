9a3185105b6aa0ee68ba1d2b9caebe0f3cc7aa99
pedrosh <serdiopedro@gmail.com>
2024-05-18 19:01:06 -0700

    chore: completed explore part

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -47,10 +47,7 @@ function addRecipesToDocument(recipes) {
  * @param {Array<Object>} recipes An array of recipes
  */
 function saveRecipesToStorage(recipes) {
-	// EXPLORE - START (All explore numbers start with B)
-	// B1. TODO - Complete the functionality as described in this function
-	//            header. It is possible in only a single line, but should
-	//            be no more than a few lines.
+	localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -58,22 +55,29 @@ function saveRecipesToStorage(recipes) {
  * <button>.
  */
 function initFormHandler() {
-	// B2. TODO - Get a reference to the <form> element
-	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
-	//            submit button is clicked
-	// Steps B4-B9 will occur inside the event listener from step B3
-	// B4. TODO - Create a new FormData object from the <form> element reference above
-	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
-	//            make this easier to read), and then extract the keys and corresponding
-	//            values from the FormData object and insert them into recipeObject
-	// B6. TODO - Create a new <recipe-card> element
-	// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-	// B8. TODO - Append this new <recipe-card> to <main>
-	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-	//            then save the recipes array back to localStorage
-	// B10. TODO - Get a reference to the "Clear Local Storage" button
-	// B11. TODO - Add a click event listener to clear local storage button
-	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	// B13. TODO - Delete the contents of <main>
+	const formEl = document.querySelector('form');
+	formEl.addEventListener('submit', function(event) {
+		event.preventDefault();
+		const formData = new FormData(formEl);
+		const recipeObject = {};
+		formData.forEach((value, key) => {
+			recipeObject[key] = value;
+		});
+		const newRecipe = document.createElement('recipe-card');
+		newRecipe.data = recipeObject;
+		document.querySelector('main').append(newRecipe);
+
+		const recipes = getRecipesFromStorage();
+		console.log(recipes)
+		recipes.push(recipeObject);
+		console.log(recipes)
+		saveRecipesToStorage(recipes); 
+		console.log(getRecipesFromStorage())
+	});
+	
+	const clearButton = document.getElementsByClassName('danger')[0];
+	clearButton.addEventListener('click', function() {
+		localStorage.clear();
+		document.querySelector('main').innerHTML = "";
+	});
 }
